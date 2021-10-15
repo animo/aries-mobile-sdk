7d9ea61633c5c347ac62b5cdf2d9ef4e5241b5ed
Karim Stekelenburg <karim@animo.id>
2021-10-15 18:45:35 +0200

    feat: add handshake protocol to ConnectionsModule
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

--- interfaces/connections/interfaces.ts
+++ interfaces/connections/interfaces.ts
@@ -12,11 +12,21 @@ interface ReceiveInvitationConfig {
   mediatorId?: string
 }
 
+enum HandshakeProtocol {
+  ConnectionProtocol,
+  DidExchange,
+}
+
+// this isn't correct and needs some revision
 interface CreateConnectionConfig {
+  protocol: HandshakeProtocol
   autoAcceptConnection?: boolean
   alias?: string
   mediatorId?: string
   multiUseInvitation?: boolean
+  goal?: string
+  // needs to have a format based on https://github.com/hyperledger/aries-rfcs/blob/main/concepts/0519-goal-codes/README.md
+  goalCode?: string
 }
 
-export { ConnectionInvitationMessage, ConnectionRecord, ReceiveInvitationConfig, CreateConnectionConfig }
+export { ConnectionInvitationMessage, ConnectionRecord, ReceiveInvitationConfig, CreateConnectionConfig, HandshakeProtocol }
