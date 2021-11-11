bd761a0a7c4aa5344463950e821e3d97fc7bfa46
Karim Stekelenburg <karim@animo.id>
2021-11-11 16:55:06 +0100

    fix: remove unnecessary docs
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

--- modules/credentials/docs/implementers-guide.md
+++ modules/credentials/docs/implementers-guide.md
@@ -63,37 +63,3 @@ One of the primary features of [issue-credential v2](https://github.com/hyperled
 
 ![envisioned-architecture](https://www.plantuml.com/plantuml/png/bPOzRzim48Pt_WeYYoODRVOWGOoYGPmb1fAkP-bY4vP8uKYrCDB-zv8GPP8IfIIRpNlkaoyFkjK6oFHjmvW-2Ta1GSixg4vmm9qvDyW9AmFoq604j4ggwaYgHMweHyQCwhXHVA-CKIslXNCIIZBcG_6h9rNtDjeMpaXN_9rnNZMWDRydh54Q0Sr0i7-HqmxfjwWIBzXhGr2PoD5J8QIISiW_jMonFY2Df_Quy8O7MPzVDiz9LRyk_vnrjtcC-3iIgE_q2MY88gH1UWDRgcD_ggOEQT8iDiwIUNS4ctOhzWGkV8hbyZnetQ_aMJ9FygHOvcAywUNRYksxSmpL1U27eXPCCih9qLR84zORCE0DIQGmpzdnK7Ye-FNw_j95hA9dx9GMHj5vJNw1Siw5rojX0qv9cas5oilo9YF2F3PJ92j6RUSoZBQomJUJzS-m90v71wSYBtW6hTKVQScvLZ-Y9rfX6P7mik_Al-kZkQXc6Svrs2SJZowC1LcDSqY2Uc5DbURdcqFpdKMJy5osfwzXS_t7tSokQiawleRm-s8Ujbd6C2wsp6Gy4R-fOYuOorbeTN4jUcMtOR4QB3YwPVrPTcZ_CtIyrdtXYUAJmjEti4YlRjjYrVn1kmliGhiPEccNiLkKjVqI-0y0 'envisioned-architecture')
 
-### Credential formats
-
-One of the main features of `issue-credential v2` is the support for multiple credential formats. Therefore, on top of the already supported Indy credential format, we will add support for JSON-LD credentials in AFJ. However, in order to allow support for other formats to be added later on, we need a mechanism to do so.
-
-<!-- TODO -->
-
-### Credential services
-
-In order to add support for `issue-credential v2`, changes to the `CredentialService` class have to be made. The following structure is a possible approach:
-
-```puml
-@startuml
-
-skinparam monochrome true
-
-abstract class CredentialService
-
-V1CredentialService -up-|> CredentialService
-V2CredentialService -up-|> CredentialService
-
-@enduml
-```
-
-#### CredentialService
-
-The _abstract_ `CredentialService` class is where overlapping functionality of `v1` and `v2` is implemented. This is mainly functionality related to the **indy credential format**.
-
-#### CredentialServiceV1
-
-This is where all `v1` specific functionality is implemented.
-
-#### CredentialServiceV2
-
-This is where all `v2` specific functionality is implemented. This service also needs a **mechanism** to add support for other formats later on.
--- modules/credentials/src/services-example.ts
+++ modules/credentials/src/services-example.ts
@@ -1,28 +1,26 @@
 import { AgentMessage } from '@aries-framework/core'
-import { ProposeCredentialOptions, AcceptProposalOptions } from './interfaces'
-import { CredentialProtocolVersion } from './CredentialProtocolVersion'
-import { CredentialExchangeRecord } from './CredentialExchangeRecord'
-
-interface CredentialServiceCreateReturnType { 
-  record: CredentialExchangeRecord;
-  message: AgentMessage
-}
+import { ProposeCredentialOptions, CredentialRecord, ProtocolVersion } from './interfaces'
 
 interface CredentialService {
-  createProposal(proposal: ProposeCredentialOptions): Promise<CredentialServiceCreateReturnType>
+  createProposal(proposal: ProposeCredentialOptions): Promise<{ record: CredentialRecord; message: AgentMessage }>
 }
 
 class CredentialServiceV1 implements CredentialService {
-  public async createProposal(proposal: ProposeCredentialOptions): Promise<CredentialServiceCreateReturnType> {
-    // should only handle the proposal.credentialFormats.indy
+  /**
+   * This method should only handle the proposal.credentialFormats.indy format since it's the only
+   * format supported by v1.0
+   */
+  public async createProposal(proposal: ProposeCredentialOptions): Promise<{ record: CredentialRecord; message: AgentMessage }> {
     return {
       /** return record and message */
     }
   }
 }
 
+
+
 class CredentialServiceV2 implements CredentialService {
-  public async createProposal(proposal: ProposeCredentialOptions): Promise<CredentialServiceCreateReturnType> {
+  public async createProposal(proposal: ProposeCredentialOptions): Promise<{ record: CredentialRecord; message: AgentMessage }> {
     // should handle all formats in proposal.credentialFormats by querying and calling
     // its corresponding handler classes.
 
@@ -33,26 +31,18 @@ class CredentialServiceV2 implements CredentialService {
 }
 
 export class CredentialModule {
-  private getService(protocolVersion: CredentialProtocolVersion) {
+  private getService(protocolVersion: ProtocolVersion) {
     const serviceMap = {
-      [CredentialProtocolVersion.V1_0]: CredentialServiceV1,
-      [CredentialProtocolVersion.V2_0]: CredentialServiceV2,
+      [ProtocolVersion.V1_0]: CredentialServiceV1,
+      [ProtocolVersion.V2_0]: CredentialServiceV2,
     }
     return new serviceMap[protocolVersion]()
   }
 
-  public async proposeCredential(options: ProposeCredentialOptions): Promise<CredentialExchangeRecord> {
+  public async proposeCredential(options: ProposeCredentialOptions): Promise<CredentialRecord> {
     const service = this.getService(options.protocolVersion)
     const { record, message } = await service.createProposal(options)
     this.messageSender.send(message)
-    return record
-  }
-
-  public async acceptProposal(options: AcceptProposalOptions): Promise<CredentialExchangeRecord> {
-    const credentialRecord = await this.credentialRepository.getById(options.credentialRecordId)
-    const service = this.getService(credentialRecord.protocolVersion)
-    const { record, message } = await service.acceptProposal(credentialRecord)
-    await this.messageSender.send(message)
     return record
   }
 }
