c5b79d578ede3478b6b5e8574260b56d72b418e4
Karim Stekelenburg <karim@animo.id>
2021-11-11 13:08:22 +0100

    docs: add services example
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

---
+++ modules/credentials/src/services-example.ts
@@ -0,0 +1,41 @@
+import { AgentMessage } from '@aries-framework/core'
+import { ProposeCredentialOptions, CredentialRecord, ProtocolVersion } from './interfaces'
+
+interface CredentialService {
+  createProposal(proposal: ProposeCredentialOptions): Promise<{ record: CredentialRecord, message: AgentMessage}>
+}
+
+
+class CredentialServiceV1 implements CredentialService {
+  public async createProposal(proposal: ProposeCredentialOptions): Promise<{ record: CredentialRecord, message: AgentMessage}> {
+    // should only handle the proposal.credentialFormats.indy
+    return { /** return record and message */ }
+ }
+}
+
+class CredentialServiceV2 implements CredentialService {
+  public async createProposal(proposal: ProposeCredentialOptions): Promise<{ record: CredentialRecord, message: AgentMessage}> {
+    // should handle all formats in proposal.credentialFormats by querying and calling
+    // its corresponding handler classes.
+
+    return { /** return record and message */ }
+  }
+}
+
+
+export class CredentialModule {
+  private getService(protocolVersion: ProtocolVersion) {
+    const serviceMap = {
+      [ProtocolVersion.V1_0]: CredentialServiceV1,
+      [ProtocolVersion.V2_0]: CredentialServiceV2,
+    }
+    return new serviceMap[protocolVersion]()
+  }
+
+  public async proposeCredential(options: ProposeCredentialOptions): Promise<CredentialRecord> {
+    const service = this.getService(options.protocolVersion)
+    const {record, message} = await service.createProposal(options)
+    this.messageSender.send(message)
+    return record
+  }
+}
